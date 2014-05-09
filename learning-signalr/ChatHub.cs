85830bcf0f93b290c64549c3e5bc0537bbef8786
Sri Velicheti <velicheti.harsha@gmail.com>
2014-05-08 22:56:13 -0400

    First Commit

---
+++ learning-signalr/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace learning_signalr
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name,string message) {
+            Clients.All.broadCastMessage(name, message);
+        }
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
