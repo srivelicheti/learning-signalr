0106b9d8f194f21415fbbf181b4b976868dcef7d
Sri Velicheti <velicheti.harsha@gmail.com>
2014-05-09 18:34:41 -0400

    Commit from 19

---
+++ learning-signalr/MoveShapeDemo/Broadcaster.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace MoveShapeDemo
+{
+    public class Broadcaster
+    {
+        private readonly static Lazy<Broadcaster> _instance = new Lazy<Broadcaster>(() => new Broadcaster());
+        private readonly TimeSpan _broadCastInterval = TimeSpan.FromMilliseconds(2000);
+        private readonly IHubContext _hubContext;
+        private Timer _broadcastLoop;
+        private Shape _model;
+        private bool _modelUpdated = false;
+
+        private Broadcaster() {
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<MoveShapeHub>();
+            _model = new Shape();
+            _broadcastLoop = new Timer(BroadcastShape,null,_broadCastInterval,_broadCastInterval );
+        }
+
+        public void BroadcastShape(object shape) {
+            if (_modelUpdated)
+            {
+                _hubContext.Clients.AllExcept(_model.LastUpdatedBy).updateShape(_model);
+                _modelUpdated = false;
+            }
+        }
+
+        public void UpdateShape(Shape clientShape)
+        {
+            _model = clientShape;
+            _modelUpdated = true;
+        }
+
+        public static Broadcaster Instance
+        {
+            get {
+                return _instance.Value;
+            }
+        }
+    }
+}
---
+++ learning-signalr/MoveShapeDemo/MoveShapeHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoveShapeDemo
+{
+    public class MoveShapeHub : Hub
+    {
+        private Broadcaster _broadcaster;
+        public MoveShapeHub():this(Broadcaster.Instance)
+        { }
+
+        public MoveShapeHub(Broadcaster broadcaster)
+        {
+            _broadcaster = broadcaster;
+        }
+        public void UpdateShape(Shape clientShape)
+        {
+            clientShape.LastUpdatedBy = Context.ConnectionId;
+            _broadcaster.UpdateShape(clientShape);
+            //Clients.AllExcept(clientShape.LastUpdatedBy).updateShape(clientShape);
+        }
+    }
+
+    public class Shape
+    {
+        [JsonProperty("left")]
+        public double Left { get; set; }
+
+        [JsonProperty("top")]
+        public double Top { get; set; }
+
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+
+    }
+}
